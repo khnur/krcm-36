a69522fc2c27c09741768043eebdbf116e0acfb4
nkozhamuratov <NKozhamuratov@one.kz>
2024-04-13 11:22:10 +0500

    first commit

---
+++ src/main/java/kz/hacknu/krcm36/Krcm36Application.java
@@ -0,0 +1,13 @@
+package kz.hacknu.krcm36;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Krcm36Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Krcm36Application.class, args);
+	}
+
+}
---
+++ src/test/java/kz/hacknu/krcm36/Krcm36ApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.hacknu.krcm36;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Krcm36ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
